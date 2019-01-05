47d2da10f7dbb6cf453c85919fd06406a3d357de
Jianbo He <hjbxyz@gmail.com>
2019-01-05 11:47:21 +0800

    Update

--- root/lock-user.sh
+++ root/lock-user.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-if [ $(id -u) -eq 0 ]; then
-    usermod -e 1 -L $1
-    rocks sync users
-else
-    echo "ERROR: NO ROOT PRIVILEGE!"
+if [ $(id -u) -ne 0 ]; then
+    echo "ERROR: permission denied"
     exit 1
 fi
+
+chage -E 0 $1
+
+rocks sync users
--- root/unlock-user.sh
+++ root/unlock-user.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-if [ $(id -u) -eq 0 ]; then
-    usermod -e 99999 -U $1
-    rocks sync users
-else
-    echo "ERROR: NO ROOT PRIVILEGE!"
+if [ $(id -u) -ne 0 ]; then
+    echo "ERROR: permission denied"
     exit 1
 fi
+
+chage -E -1 $1
+
+rocks sync users
