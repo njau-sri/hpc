f206bb397127a03cd78edadee8259749bc3cb1af
Jianbo He <hjbreg@users.noreply.github.com>
2019-03-13 13:40:09 +0800

    Update install-jre.sh

--- root/install-jre.sh
+++ root/install-jre.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-tar xzf jdk-8u201-linux-x64.tar.gz -C /share/apps/java/
+tar xzf jre-8u201-linux-x64.tar.gz -C /share/apps/java/
 
 cd /share/apps/java
 
 rm -rf latest
 
-ln -s jdk1.8.0_201 latest
+ln -s jre1.8.0_201 latest
 
 cd
