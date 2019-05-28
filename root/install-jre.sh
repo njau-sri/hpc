1bb7d453c175dfeaa650d19ca685eb8ee01529d6
Jianbo He <hjbxyz@gmail.com>
2019-05-28 11:08:12 +0800

    Update install-jre.sh

--- root/install-jre.sh
+++ root/install-jre.sh
@@ -5,3 +5,4 @@ PREFIX=/share/apps/java/jre
 tar zxf OpenJDK11U-jre_x64_linux_hotspot_11.0.3_7.tar.gz -C $PREFIX
 
 echo 'export PATH=/share/apps/java/jre/jdk-11.0.3+7-jre/bin:$PATH' > $PREFIX/env.sh
+echo 'java -version' > $PREFIX/env.sh
