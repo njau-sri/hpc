ccaa610e790a398495b34429ad890919a6e40c11
Jianbo He <hjbxyz@gmail.com>
2019-05-29 12:10:03 +0800

    Update xpclr

--- qsub/xpclr/xpclr.sh
+++ qsub/xpclr/xpclr.sh
@@ -8,11 +8,32 @@
 #
 
 . /share/apps/python/3.7.3/env.sh
+. /share/apps/htslib/1.9/env.sh
 
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
 
+mkdir xpclr-output
+output=$(pwd)/xpclr-output
+
 # your code goes here
 
-xpclr --out $HOME/xpclr --format vcf --input snp.vcf --samplesA samplesA.txt --samplesB samplesB.txt --map map.txt
+# vcf file
+vcf=snp.vcf.gz
+
+# --map  space delimited file. 6 columns: ID, chromosome, Genetic Distance, Position, REF, ALT
+map=map.txt
+
+# sample list file
+samplesA=samplesA.txt
+samplesB=samplesB.txt
+
+for chr in 1 2
+do
+
+xpclr --chr $chr --out $output/$chr --input $vcf --map $map \
+--samplesA $samplesA --samplesB $samplesB \
+--maxsnps 2000 --minsnps 2 --size 100000 --step 50000
+
+done
