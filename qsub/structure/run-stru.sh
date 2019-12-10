903396a9fb26cd1a0c95cdd1d74f63f171356688
Jianbo He <hjbxyz@gmail.com>
2019-12-10 09:51:00 +0800

    Update structure

--- qsub/structure/run-stru.sh
+++ qsub/structure/run-stru.sh
@@ -7,30 +7,46 @@
 #$ -l mem_free=4G
 #
 
+
 . /share/apps/plink/env.sh
 . /share/apps/structure/env.sh
 
+
 # your code goes here
+
 
 MAXPOPS=5
 NUMREPS=3
 
+
+# genotype file in VCF format
 vcf_file=snp.vcf
 
 
+# LD-based SNP pruning
 plink --allow-extra-chr --vcf $vcf_file --indep-pairwise 50 10 0.1
 plink --allow-extra-chr --vcf $vcf_file --extract plink.prune.in --recode12 --out pruned
 
 
+# prepare STRUCTURE infile
 NUMINDS=$(wc -l pruned.ped | cut -d " " -f 1)
 NUMLOCI=$(wc -l pruned.map | cut -d " " -f 1)
 cut -d " " -f 2,7- pruned.ped > infile
 
 
+# create output dir
+rm -rf seed.txt
+mkdir structureoutput
+
+
+# run STRUCTURE
 for K in $(seq $MAXPOPS)
 do
     for R in $(seq $NUMREPS)
     do
-        structure -L $NUMLOCI -N $NUMINDS -K $K -o outfile_K${K}_R${R}
+        structure -L $NUMLOCI -N $NUMINDS -K $K -o structureoutput/outfile_K${K}_R${R}
     done
 done
+
+
+# python structureHarvester.py --evanno --clumpp --out harvesteroutput --dir structureoutput
