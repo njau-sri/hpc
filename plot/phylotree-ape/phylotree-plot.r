4063ad0eea9afa91b39f440cb8875e4a7f74fd1c
Jianbo He <hjbxyz@gmail.com>
2020-01-14 11:16:16 +0800

    update

--- plot/phylotree-ape/phylotree-plot.r
+++ plot/phylotree-ape/phylotree-plot.r
@@ -1,8 +1,18 @@
 library(ape)
 
-tr <- read.tree("tree.nwk")
+tr <- read.tree("njtree.nwk")
 
-print(summary(tr$edge.length))
+len <- median(tr$edge.length)
+
+if (len < 0.1) {
+    len <- floor(len*100)/100
+} else if (len < 1) {
+    len <- floor(len*10)/10
+} else {
+    len <- floor(len)
+}
+
+print(len)
 
 # tr$tip.label
 # tr <- root(tr, "Struthioniformes")
@@ -13,6 +23,6 @@ par(lend=2)
 
 plot(tr, type="un", show.tip.label=FALSE, no.margin=TRUE)
 
-add.scale.bar(length=5)
+add.scale.bar(length=len)
 
 dev.off()
